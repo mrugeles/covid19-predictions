c77cc260e59c769cd9500b93936fffa51bc3a314
Mario Rugeles <mrugeles@gmail.com>
2020-07-26 14:31:58 -0500

    Adding countriy selection

--- application.py
+++ application.py
@@ -8,27 +8,31 @@ application = Flask(__name__)
 api = Api(application)
 
 s3 = boto3.client('s3')
+s3_bucket = 'co.data.covid19-us-east-2'
 
 
 def load_dataframe(file_name):
-    obj = s3.get_object(Bucket='co.data.covid19-us-east-2', Key=f'countries/{file_name}')
+    obj = s3.get_object(Bucket=s3_bucket, Key=file_name)
     return pd.read_csv(obj['Body'])
 
 
 @application.route('/')
 @application.route('/index')
 def index():
-    confirmed_df = load_dataframe('Colombia_confirmed.csv')
-    confirmed_df.fillna(0, inplace=True)
-    deaths_df = load_dataframe('Colombia_deaths.csv')
-    recovered_df = load_dataframe('Colombia_recovered.csv')
+    countries_df = load_dataframe('processed_countries.csv')
 
-    predicted_confirmed_df = load_dataframe('Colombia_predicted_confirmed.csv')
-    predicted_deaths_df = load_dataframe('Colombia_predicted_deaths.csv')
-    predicted_recovered_df = load_dataframe('Colombia_predicted_recovered.csv')
+    confirmed_df = load_dataframe('countries/Colombia_confirmed.csv')
+    confirmed_df.fillna(0, inplace=True)
+    deaths_df = load_dataframe('countries/Colombia_deaths.csv')
+    recovered_df = load_dataframe('countries/Colombia_recovered.csv')
+
+    predicted_confirmed_df = load_dataframe('countries/Colombia_predicted_confirmed.csv')
+    predicted_deaths_df = load_dataframe('countries/Colombia_predicted_deaths.csv')
+    predicted_recovered_df = load_dataframe('countries/Colombia_predicted_recovered.csv')
 
     return render_template(
         'index.html',
+        countries = list(countries_df['country'].values),
         labels=list(confirmed_df['date'].values),
         predicted_labels=list(predicted_confirmed_df['date'].values),
         confirmed=list(confirmed_df['daily_cases'].values),
