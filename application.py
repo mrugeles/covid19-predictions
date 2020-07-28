027e9481ef49a0755ed9ec64d742f69bcc041a76
Mario Rugeles <mrugeles@gmail.com>
2020-07-27 21:56:49 -0500

    Fix plot date labels

--- application.py
+++ application.py
@@ -21,10 +21,12 @@ def load_dataframe(file_name):
 def index():
     country = request.args.get('country', 'Colombia')
     countries_df = load_dataframe('processed_countries.csv')
-    print(f'countries/{country}_confirmed.csv')
+
     confirmed_df = load_dataframe(f'countries/{country}_confirmed.csv')
     confirmed_df.fillna(0, inplace=True)
+
     deaths_df = load_dataframe(f'countries/{country}_deaths.csv')
+    print(deaths_df.tail())
     recovered_df = load_dataframe(f'countries/{country}_recovered.csv')
 
     predicted_confirmed_df = load_dataframe(f'countries/{country}_predicted_confirmed.csv')
@@ -35,17 +37,13 @@ def index():
         'index.html',
         country=country,
         countries=list(countries_df['country'].values),
-        labels=list(confirmed_df['date'].values),
-        predicted_labels=list(predicted_confirmed_df['date'].values),
-        confirmed=list(confirmed_df['daily_cases'].values),
-        deaths=list(deaths_df['daily_cases'].values),
-        recovered=list(recovered_df['daily_cases'].values),
-        acumulated_confirmed=list(confirmed_df['y'].astype(float).values),
-        acumulated_deaths=list(deaths_df['y'].astype(float).values),
-        acumulated_recovered=list(recovered_df['y'].astype(float).values),
-        predictions_confirmed=list(predicted_confirmed_df['y'].astype(float).values),
-        predictions_deaths=list(predicted_deaths_df['y'].astype(float).values),
-        predictions_recovered=list(predicted_recovered_df['y'].astype(float).values),
+        confirmed=confirmed_df,
+        deaths=deaths_df,
+        recovered=recovered_df,
+
+        predictions_confirmed=predicted_confirmed_df,
+        predictions_deaths=predicted_deaths_df,
+        predictions_recovered=predicted_recovered_df
     )
 
 
